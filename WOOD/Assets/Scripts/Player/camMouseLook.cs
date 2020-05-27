e57fc3831c66b8c216e33f83c7604d6ee260380c
EtCtulu <bertrandemilien17@gmail.com>
2020-05-27 19:12:46 +0200

    Ajout du projet Unity, création d'un scène test, ajout d'un first person controller, caméra fonctionelle, jump fonctionnel avec un IsGrounded()

---
+++ WOOD/Assets/Scripts/Player/camMouseLook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camMouseLook : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothView;
+
+    [Header("Variables")]
+    // Variables de Sensiilité et de Smoothing
+    public float sensitivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    // Le Character
+    GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        // Mouvements de la camera
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothView.x = Mathf.Lerp(smoothView.x, md.x, 1f / smoothing) ;
+        smoothView.y = Mathf.Lerp(smoothView.y, md.y, 1f / smoothing);
+        mouseLook += smoothView;
+
+        // Clamp la view
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90f, 90f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ WOOD/Assets/Scripts/Player/characterController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    [Header("Variables")]
+    // Vitesse du joueur
+    public float speed = 10f;
+
+    // Force du Jump
+    public float jumpForce = 7;
+
+    // La layer pour le groundcheck
+    public LayerMask groundLayer;
+
+    // Raccourcis
+    public CapsuleCollider col;
+    private Rigidbody rb;
+
+
+
+    void Start()
+    {
+        // Curseur bloqué au centre du joueur
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Déclaration des raccourcis
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // L'axis vertical
+        float translation = Input.GetAxis("Vertical") * speed;
+
+        // L'axis horizontal
+        float straffe = Input.GetAxis("Horizontal") * speed;
+
+        // Permets de ne pas avoir un vitesse diff suivant les frames
+        translation *= Time.deltaTime;
+
+        // Pareil pour le straffe
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+
+        // Débloquage de la souris
+        if (Input.GetKeyDown("escape"))
+        {
+
+            // Enlève le bloquage du curseur
+            Cursor.lockState = CursorLockMode.None;
+
+        }
+
+        // Le saut
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            // La force du saut
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+
+        }
+    }
+
+    // Le check du ground
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y
+            , col.bounds.center.z), col.radius * .9f, groundLayer);
+    }
+
+    
+}
