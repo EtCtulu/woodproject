c1887b80430ad1a88c309cad71f7a7361a6596ce
EtCtulu <bertrandemilien17@gmail.com>
2020-05-28 18:13:15 +0200

    Des débuts de raycast, c'est pas encore ça

--- WOOD/Assets/Scripts/Enemy/Base Imp/playerDetector.cs
+++ WOOD/Assets/Scripts/Enemy/Base Imp/playerDetector.cs
@@ -6,28 +6,73 @@ public class playerDetector : MonoBehaviour
 {
     // Le bool qui dit si le joueur est en range ou pas 
     public bool playerInRange = false;
+    // Si le joueur est dans le carré, ça autorise le raycast
+    private bool playerinSquare = false;
+
+    // Declaration du personage
+    private GameObject player;
+
+    private GameObject pointer;
+    
+
     void Start()
     {
-        
+        // On déclare le joueur
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // Declaration du raycast
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        pointer = this.transform.Find("ray").gameObject;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        transform.LookAt(player.transform);
+
+        pointer.transform.LookAt(player.transform);
         
+        // Création du layermask
+        int layerMask = 1 << 8;
+
+        layerMask = ~layerMask;
+
+        // La valeur hit du raycast
+        RaycastHit hit;
+
+        if (playerinSquare == true)
+        {
+            if (Physics.Raycast(transform.position, player.transform.position, out hit, Mathf.Infinity, layerMask)) //Range infinie
+            {
+                Debug.DrawRay(transform.position, player.transform.position, Color.cyan);
+                if(hit.collider.tag == "Player")
+                {
+                    playerInRange = true;
+                }
+            }
+            else
+            {
+                playerInRange = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            playerInRange = true;
+            playerinSquare = true;
+            // playerInRange = true;
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
+            playerinSquare = false;
             playerInRange = false;
         }
     }
