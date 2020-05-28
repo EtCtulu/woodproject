8e7f706ee93f5fccec2ce515e9b9b4f838ffeb88
EtCtulu <bertrandemilien17@gmail.com>
2020-05-29 00:32:03 +0200

    Ajout d'un début d'UI et des esquisses d'un système de changement d'armes, la suite, la vie !

--- WOOD/Assets/Scripts/Player/weaponRaycast.cs
+++ WOOD/Assets/Scripts/Player/weaponRaycast.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class weaponRaycast : MonoBehaviour
 {
@@ -10,16 +11,31 @@ public class weaponRaycast : MonoBehaviour
 
     // Les munitions des armes
     [Header("Ammo")]
-    public float pistolAmmo = 100;
+    public int pistolAmmo = 100;
+
+    // Ui pour les munitions
+    GameObject canvas;
+    public Text ammoText;
+
+    // Les variables qui se modifient au changement de l'arme
+    int equipedWpnAmmo;
+    string equipedWpn;
+
+    // Les bools des armes
+    bool pistol = true;
 
     void Start()
     {
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+        ammoText = canvas.transform.Find("ammo").gameObject.GetComponent<Text>();
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
     }
 
     // Update is called once per frame
     void Update()
     {
+        ammoText.text = equipedWpn + equipedWpnAmmo;
+
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
         // Bit shift the index of the layer (8) to get a bit mask
         int layerMask = 1 << 8;
@@ -35,10 +51,23 @@ public class weaponRaycast : MonoBehaviour
         // Permets de voir le raycast du pistolet et sa range, il est en rouge
         Debug.DrawRay(transform.position, transform.forward * 30, Color.red);
 
+        // L'equipement du pistolet et ce qui suffit
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            pistol = true;
+            equipedWpn = "Pistol : ";
+        }
+        if(pistol == true)
+        {
+            equipedWpnAmmo = pistolAmmo;
+            equipedWpn = "Pistol : ";
+        }
+
+
         if (Input.GetButtonDown("Fire1"))
         {
             // Raycast du pistolet, il as une range de 30, et on peut tirer que si on as des balles
-            if (Physics.Raycast(transform.position, transform.forward, out hit, 30, layerMask) && pistolAmmo != 0) 
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 30, layerMask) && pistolAmmo != 0 && pistol == true) 
             {
                 // Les munitions qui décrémentes
                 pistolAmmo--;
