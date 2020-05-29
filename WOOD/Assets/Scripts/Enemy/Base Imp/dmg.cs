3475303036c167c2fd7d38f86338ff291cd26bf9
EtCtulu <bertrandemilien17@gmail.com>
2020-05-29 17:44:21 +0200

    Prise de dégats + debut de l'ui des hps.

---
+++ WOOD/Assets/Scripts/Enemy/Base Imp/dmg.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dmg : MonoBehaviour
+{
+
+    // Les dégats de la melee, touts les scipts doivent avoir le nom comme ça.
+    [Header("Damage")]
+    public float damageMelee = 20;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WOOD/Assets/Scripts/Player/characterHP.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class characterHP : MonoBehaviour
+{
+    // Déclaration des scripts de Vie et d'armure
+    [Header("Health And Armor")]
+    public float maxHp = 100;
+    public float hp = 100;
+    public float armor = 0;
+
+    [Header("Barre de vie")]
+    public Image hpBar;
+    private float ratio;
+
+    // Le float qui changera en fonction des dégats
+    private float dmgToTake;
+
+    void Start()
+    {
+        Debug.Log("Vie : " + hp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ratio = hp / maxHp;
+        hpBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Le script pour les dégats de Melee
+       if(other.gameObject.tag == "Melee")
+        {
+            dmgToTake = other.GetComponent<dmg>().damageMelee;
+
+            hp = hp - dmgToTake;
+
+            Debug.Log("Vie : " + hp);
+
+        } 
+    }
+}
